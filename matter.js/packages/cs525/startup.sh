3a3554c1052b2656a07e3e3f2b29cd23f06c6b1b
Minh Duong <git@whitehoodhacker.net>
2025-04-06 16:48:12 -0500

    update startup script to also start the VMB

---
+++ matter.js/packages/cs525/startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run from the cs525 directory
+
+npm run build
+
+# Now startup the VMB
+tmux new -d "/bin/sh -c 'node dist/esm/VirtualMatterBrokerNode.js -- --storage-clear'"
+
+# 20 devices
+for i in {10..29}; do
+  # Try to start the server
+  echo "Starting sensor $i..."
+  DEVICE_ID=$i node dist/esm/SensorDeviceNode.js &
+done
