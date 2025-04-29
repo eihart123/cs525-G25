6d2fce67492c49aa543bd376b4bdc604532dc501
Pete Stenger <pete@stenger.io>
2025-04-28 19:48:03 -0500

    clean

--- matter.js/packages/cs525-baseline/clean.sh
+++ matter.js/packages/cs525-baseline/clean.sh
@@ -1,6 +1,8 @@
 set -x #echo on
 
 killall node
+rm -rf /opt/matter/cs525-G25/matter.js/packages/cs525/*.log || true
+rm -rf /opt/matter/cs525-G25/matter.js/packages/cs525-baseline/*.log || true
 rm -rf ~/.matter/controller-baseline
 rm -rf ~/.matter/1*
 rm -fr ~/.matter/device-*
